751ae254b2269ef573475e1f1b39ebf9e7f1785e
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-12-17 00:12:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let toggleBar = document.querySelector(".toggle-bars");
+let navLinks = document.querySelector(".navLinks");
+let icone = toggleBar.querySelector("i");
+let links = navLinks.querySelectorAll("a")
+
+
+toggleBar.addEventListener("click", ()=>{
+    navLinks.classList.toggle("open")
+    icone.classList.toggle("fa-bars")
+    icone.classList.toggle("fa-xmark")
+})
+
+links.forEach(links =>{
+        links.addEventListener("click", ()=>{
+            navLinks.classList.remove("open")
+            icone.classList.remove("fa-xmark")
+            icone.classList.add("fa-bars")
+        })
+})
