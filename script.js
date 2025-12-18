9f90e506202001e60fc695b8ed9abb9e9dc0600b
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-12-17 22:38:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -20,35 +20,3 @@ links.forEach(links =>{
 
  AOS.init();
 
-const scrollRevealOption = {
-    distance: "50px",
-    origin: "bottom",
-    duration: 2000,
-}
-
-ScrollReveal().reveal(".logo", {
-    ...scrollRevealOption,
-     origin:"left",
-    delay: 600,
-})
-
-ScrollReveal().reveal(".navLinks", {
-    ...scrollRevealOption,
-     origin:"top",
-    delay: 700,
-    interval:500,
-})
-
-ScrollReveal().reveal(".navLinks a", {
-    ...scrollRevealOption,
-     origin:"top",
-    delay: 750,
-    interval:600,
-})
-
-ScrollReveal().reveal(".texto-principal", {
-    ...scrollRevealOption,
-     origin:"left",
-    delay: 800,
-})
-
