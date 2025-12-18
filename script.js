2371daaf12923eeb5363d77ff288987c04fb738b
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-12-17 22:30:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,3 +17,38 @@ links.forEach(links =>{
             icone.classList.add("fa-bars")
         })
 })
+
+ AOS.init();
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 2000,
+}
+
+ScrollReveal().reveal(".logo", {
+    ...scrollRevealOption,
+     origin:"left",
+    delay: 600,
+})
+
+ScrollReveal().reveal(".navLinks", {
+    ...scrollRevealOption,
+     origin:"top",
+    delay: 700,
+    interval:500,
+})
+
+ScrollReveal().reveal(".navLinks a", {
+    ...scrollRevealOption,
+     origin:"top",
+    delay: 750,
+    interval:600,
+})
+
+ScrollReveal().reveal(".texto-principal", {
+    ...scrollRevealOption,
+     origin:"left",
+    delay: 800,
+})
+
